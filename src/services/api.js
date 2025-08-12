df59b034215260a2e32e0d400e37aef3136feeb3
Wendel <wendel.m.veloso@hotmail.com>
2025-08-11 21:31:37 -0300

    fix: adjust environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,13 @@
 import axios from "axios";
-import {getItem} from "../utils/storage"
+import { getItem } from "../utils/storage";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 10000,
   headers: {
-    "Access-Control-Allow-Origin": "*",
     "X-Requested-With": "XMLHttpRequest",
     Authorization: getItem("token") ? `Bearer ${getItem("token")}` : "",
-  timeout: 10000,
   },
 });
 
 export default api;
- 
